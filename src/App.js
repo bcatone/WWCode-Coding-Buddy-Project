7768a9f9c87de61bcb7ae9fdda0996b0f229cc30
Brittany Laura Catone <bcatone@outlook.com>
2023-06-12 00:10:29 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React, { useState, useEffect }  from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +27,9 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <p>The current time is {currentTime}.</p>
+
     </div>
   );
 }
